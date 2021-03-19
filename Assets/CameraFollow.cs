4c535cd1dfbace26d6a45606d4553694c3d91fb5
hannahbish <510894@chrome.escg.ac.uk>
2021-03-19 11:59:46 +0000

    test

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+       
+        if (target != null)
+        {
+            Vector3 desiredposition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothSpeed);
+            transform.position = smoothedPosition;
+
+
+        }
+
+    }
+
+
+
+
+}
+
+
+
